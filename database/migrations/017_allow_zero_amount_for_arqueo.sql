2df98d9f098cc42652e6f185f9ebb0e7f1db6479
adriprimomo-hub <primomoadriano@gmail.com>
2026-02-23 23:52:49 -0300

    Delete database directory

--- database/migrations/017_allow_zero_amount_for_arqueo.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE caja_movimientos
-DROP CONSTRAINT IF EXISTS caja_movimientos_monto_check;
-
-ALTER TABLE caja_movimientos
-ADD CONSTRAINT caja_movimientos_monto_check
-CHECK (monto > 0 OR (monto = 0 AND source_tipo = 'arqueo'));
